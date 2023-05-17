088ae59d1b491cbae0e2987a8d09aefe607b22be
Taflnne15 <taflynnepetersen@gmail.com>
2023-05-17 09:38:17 +0200

    DAY3

---
+++ script/code.js
@@ -0,0 +1,17 @@
+let data = 'Taflynne'
+console.log(typeof data)
+
+data = 5
+console.log(typeof data)
+
+let gender = true
+console.log(typeof gender)
+
+let firstName = undefined
+console.log(typeof firstName)
+
+let lastName = null
+console.log(typeof lastName)
+
+
+
